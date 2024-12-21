cddf0323bd63de2f7b15e41dde1b642f0d245f14
blackusb <smpaa@naver.com>
2024-12-21 18:06:34 +0900

    Delete src directory

--- src/j02_array/Array03.java
+++
@@ -1,9 +0,0 @@
-package j02_array;
-
-public class Array03 {
-    public static void main(String[] args){
-        //main()메소드의 String배열 매개변수(arguments)는 클래스 실행시 외부에서
-        //클래스내로 문자데이터를 가져올 수 있다.
-        System.out.println("main()메소드 매개변수 테스트");
-    }
-}
